7700d8833e9a018ca1e0856882ecd459116a0a83
Edwin Lunandy <populationtwo@gmail.com>
2015-03-17 10:01:03 -0400

    Add base styling to the form.

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-scss-lint' );
 
 	// Default task.
 	grunt.registerTask( 'default', ['concat', 'uglify', 'jshint'] );
